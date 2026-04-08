46823ce6eb31b2573109f5abe63210e5317953a4
minaripenguin <minaripenguin@users.noreply.github.com>
2026-04-09 00:01:57 +0200

    colors: Introduce Reality display engine [2/2]
    Change-Id: Ib565b3ffaa8a5f5832d5a620961abffd754e2dfc
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/display/ColorModePreferenceFragment.java
+++ src/com/android/settings/display/ColorModePreferenceFragment.java
@@ -32,6 +32,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings.Secure;
 import android.util.Log;
 import android.view.View;
@@ -223,6 +224,20 @@ public class ColorModePreferenceFragment extends RadioButtonPickerFragment {
             mViewPager.setCurrentItem(mPageList.size() - 1, true);
         }
         updateIndicator(mViewPager.getCurrentItem());
+    }
+
+    private static final String KEY_DISPLAY_ENGINE_CATEGORY = "display_engine_category";
+
+    @Override
+    public void updateCandidates() {
+        super.updateCandidates();
+
+        PreferenceScreen screen = getPreferenceScreen();
+        if (ColorDisplayManager.isColorTransformAccelerated(screen.getContext())
+                && screen.findPreference(KEY_DISPLAY_ENGINE_CATEGORY) == null) {
+            getPreferenceManager().inflateFromResource(
+                    screen.getContext(), R.xml.display_engine_settings, screen);
+        }
     }
 
     @Override
@@ -418,6 +433,22 @@ public class ColorModePreferenceFragment extends RadioButtonPickerFragment {
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.color_mode_settings) {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final List<SearchIndexableResource> resources = new ArrayList<>();
+                    final SearchIndexableResource colorModeXml = new SearchIndexableResource(context);
+                    colorModeXml.xmlResId = R.xml.color_mode_settings;
+                    resources.add(colorModeXml);
+                    if (ColorDisplayManager.isColorTransformAccelerated(context)) {
+                        final SearchIndexableResource displayEngineXml =
+                                new SearchIndexableResource(context);
+                        displayEngineXml.xmlResId = R.xml.display_engine_settings;
+                        resources.add(displayEngineXml);
+                    }
+                    return resources;
+                }
 
                 @Override
                 protected boolean isPageSearchEnabled(Context context) {
